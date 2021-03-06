33bc4a8a02afdca7010899b29db0746807da4efc
Andrew Fillenwarth <andyf016@gmail.com>
2021-03-06 12:31:04 -0500

    setting up page py

--- testcase/main.py
+++ testcase/main.py
@@ -9,8 +9,9 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome("/Users/andrewfillenwarth/Desktop/Projects/selenium/chromedriver")
         self.driver.get("http://www.python.org")
     
-    def test_example():
-
+    def test_title(self):
+        mainPage = page.MainPage()
+        assert mainPage.is_title_matches()
 
     def tearDown(self):
         self.driver.close()
