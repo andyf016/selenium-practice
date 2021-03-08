40cb55ec279c1fe2dd41934e6616e4e61d2c2ed5
Andrew Fillenwarth <andyf016@gmail.com>
2021-03-08 00:39:54 -0500

    fixed some atr errors, testing properly

--- testcase/locator.py
+++ testcase/locator.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocator(object):
+class MainPageLocators(object):
+    """A class for main page locators. All main page locators should come here"""
     GO_BUTTON = (By.ID, 'submit')
 
-class SearchResultsPageLocator(object):
+class SearchResultsPageLocators(object):
+    """A class for search results locators. All search results locators should come here"""
     pass
