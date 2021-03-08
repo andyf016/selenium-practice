4610f8fc7a330958df484d95e056df5b2ee68c67
Andrew Fillenwarth <andyf016@gmail.com>
2021-03-07 23:49:54 -0500

    set up element py

---
+++ testcase/element.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasePageElement(object):
+    def __set__(self, obj, value):
+        driver = obj.driver
+        WebDriverWait(driver, 100).until(
+            lambda driver: driver.find_element_by_name(self.locator))
+            driver.find_element_by_name(self.locator).clear()
+            driver.find_element_by_name(self.locator).send_keys(value)
